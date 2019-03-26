52d55a347203d9f11ed46c2d3e0ad578074c3268
Benjamin Karic <b_kari02@uni-muenster.de>
2019-03-26 18:35:54 +0100

    outsource routing and create navbar, make use of angular material

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { LandingpageComponent } from '../landingpage/landingpage.component';
+import { PhenomenaComponent } from '../phenomena/phenomena.component';
+import { SensorsComponent } from '../sensors/sensors.component';
+import { DevicesComponent } from '../devices/devices.component';
+import { PhenomenaDetailComponent } from '../phenomena-detail/phenomena-detail.component';
+import { PhenomenaFormComponent } from '../phenomena-form/phenomena-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SensorsDetailComponent } from '../sensors-detail/sensors-detail.component';
+import { DevicesDetailComponent } from '../devices-detail/devices-detail.component';
+import { SensorsFormComponent } from '../sensors-form/sensors-form.component';
+import { DevicesFormComponent } from '../devices-form/devices-form.component';
+import { DomainsComponent } from '../domains/domains.component';
+import { DomainsDetailComponent } from '../domains-detail/domains-detail.component';
+import { DomainsFormComponent } from '../domains-form/domains-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingpageComponent
+  },
+  {
+    path: 'phenomena', 
+    component: PhenomenaComponent},
+  {
+    path: 'phenomenon/:iri',
+    component: PhenomenaDetailComponent},
+  {
+    path: 'phenomenonupdate/:iri',
+    component: PhenomenaFormComponent},
+  {
+    path: 'sensors',
+    component: SensorsComponent},
+  {
+    path: 'sensor/:iri',
+    component: SensorsDetailComponent},
+  {
+    path: 'sensorupdate/:iri',
+    component: SensorsFormComponent},
+  {
+    path: 'devices',
+    component: DevicesComponent},
+  {
+    path: 'device/:iri', 
+    component: DevicesDetailComponent},
+  {
+    path: 'deviceupdate/:iri', 
+    component: DevicesFormComponent},
+  {
+    path: 'domains',
+    component: DomainsComponent},
+  {
+    path: 'domain/:iri', 
+    component: DomainsDetailComponent},
+  {
+    path: 'domainupdate/:iri', 
+    component: DomainsFormComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+     RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'senph-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+
+  fillerNav: Array<{path: string, title: any}> = [
+    {
+      path: 'phenomena', 
+      title: 'All phenomena'},
+    {
+      path: 'sensors',
+      title: 'All sensors'},
+    {
+      path: 'devices',
+      title: 'All devices'},
+    {
+      path: 'domains',
+      title: 'All domains'}
+  ];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
   }
