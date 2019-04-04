ab8299f465a375ad6aa0855a21cd305479ef4152
BKaric <b_kari02@uni-muenster.de>
2019-04-04 13:04:25 +0200

    add sensor form template, edit phenomena page to show no blanknodes

--- src/app/phenomenon.ts
+++ src/app/phenomenon.ts
@@ -14,4 +14,26 @@ export class Phenomenon {
     ) {  }
   
   }
+  export class Sensor {
+
+    constructor(
+        public name: {
+          label: string,
+          lang: string
+        },
+        public description: {
+                comment:string,
+                lang: string
+        }  ,
+        public sensorElement: [{ 
+           phenomenon: string,
+           uoa: number
+         }],
+        public manufacturer: string,
+        public price: number,
+        public dataSheet: string,
+        public lifePeriod: number       
+    ) {  }
+  
+  }
   
