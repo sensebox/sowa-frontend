d945fa28c42f1b477e30e72428430a4bb0caf619
Benjamin Karic <b_kari02@uni-muenster.de>
2019-05-15 17:17:48 +0200

    restructure phenomena details page, styling

--- src/app/phenomena-detail/phenomena-detail.component.ts
+++ src/app/phenomena-detail/phenomena-detail.component.ts
@@ -5,15 +5,17 @@ import { ApiService } from '../services/api.service';
 @Component({
   selector: 'senph-phenomena-detail',
   templateUrl: './phenomena-detail.component.html',
-  styleUrls: ['./phenomena-detail.component.scss']
+  styleUrls: ['./phenomena-detail.component.scss', '../app.component.scss']
 })
 export class PhenomenaDetailComponent implements OnInit {
 
   phenomenon;
-  sensors = {iri: []};
-  domains = {iri: []};
-  units = {iri: []};
-  uri = {iri: [], labels: [], descriptions : []};
+  sensors = [];
+  domains = [];
+  units = [];
+  uri = []; 
+  labels = []; 
+  descriptions = [];
 
   constructor(
     private route:ActivatedRoute,
@@ -41,24 +43,24 @@ export class PhenomenaDetailComponent implements OnInit {
       console.log(res);
       res.forEach(element => {
         if(element.sensors != undefined){
-            this.sensors.iri.push(element.sensors.value);
+          this.sensors.push({ iri: element.sensors.value});
         }
         else{
           if(element.domains != undefined){
-              this.domains.iri.push(element.domains.value);
+              this.domains.push({ iri: element.domains.value});
           }
           else{
             if(element.units != undefined){
-                this.units.iri.push(element.units.value);
+                this.units.push({ iri: element.units.value});
             }
             else{
               if(element.iri != undefined){
-                  this.uri.iri.push(element.iri.value);
-                  this.uri.labels.push(element.label);
+                  this.uri.push({ iri: element.iri.value});
+                  this.labels.push({ iri: element.label.value});
               } 
               else{
                 if(element.description != undefined){
-                    this.uri.descriptions.push(element.description.value);
+                    this.descriptions.push({ iri: element.description.value});
                 }
               }
             } 
@@ -87,7 +89,7 @@ export class PhenomenaDetailComponent implements OnInit {
       // });
       // this.labels=JSON.parse(res[0].labels.value);
       // console.dir(this.labels); 
-      console.log(this.uri);
+      console.log(this.domains);
       this.phenomenon=res; 
       })
     });
