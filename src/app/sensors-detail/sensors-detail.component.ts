7bce1b4168fcf04fc1a0f5cc2b42b5dc43f989a8
Benjamin Karic <b_kari02@uni-muenster.de>
2019-05-15 17:18:53 +0200

    make list from table on sensors page and create wiki-like sensors details page

--- src/app/sensors-detail/sensors-detail.component.ts
+++ src/app/sensors-detail/sensors-detail.component.ts
@@ -5,7 +5,7 @@ import { ApiService } from '../services/api.service';
 @Component({
   selector: 'senph-sensors-detail',
   templateUrl: './sensors-detail.component.html',
-  styleUrls: ['./sensors-detail.component.scss']
+  styleUrls: ['./sensors-detail.component.scss', '../app.component.scss']
 })
 export class SensorsDetailComponent implements OnInit {
 
@@ -14,8 +14,8 @@ export class SensorsDetailComponent implements OnInit {
   lifeperiod= {value: []};
   image = {value: []};
   datasheet= {value: []};
-  phenomena = {uri: [], unit: []};
-  uri = {iri: [], labels: [], descriptions : []};
+  phenomena = [];
+  uri = {iri: [], labels: [], descriptions : [], short: []};
   max;
   constructor(
     private route:ActivatedRoute,
@@ -24,17 +24,84 @@ export class SensorsDetailComponent implements OnInit {
 
   ngOnInit() {
   this.getSensorDetails();
+
+// Avoid `console` errors in browsers that lack a console.
+var method;
+var noop = function () {};
+var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+];
+var length = methods.length;
+// var console = (window.console = window.console || {});
+
+while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+        console[method] = noop;
+    }
+}
+
+// Place any jQuery/helper plugins in here.
+
+// $(document).ready(function(){
+// $(".contentsPanel").each(function() {                
+//   $(this).prepend('<div class="hidePanel">[hide]</div><div class="showPanel">[show]</div>');
+// });
+
+
+//   $(".hidePanel").click(function(){
+//   $( this ).siblings('ul').hide( 150, function() { 
+//     $(this).parent().addClass('minimizedPanel');
+//   });
+//   });
+//   $(".showPanel").click(function(){
+//   $( this ).siblings('ul').show( 150, function() { 
+//     $(this).parent().removeClass('minimizedPanel');
+//   });
+//   });
+
+
+// });
+
+
+  }
+
+  relativeLink(id){
+    console.log("/sensor/"+ this.uri.iri.slice(33) );
   }
 
   getSensorDetails() {
   this.route.params.subscribe(res => {
-    var q = res.iri.replace("http://www.opensensemap.org/SENPH#", "");
-    this.api.getSensorIRI(q).subscribe((res:Array<any>) => {
+    // var q = res.iri.replace("http://www.opensensemap.org/SENPH#", "");
+    this.api.getSensorIRI(res.iri).subscribe((res:Array<any>) => {
       res.forEach(element => {
         switch (Object.getOwnPropertyNames(element)[0]) {
           
           case "price" : {
-            this.price.value.push(element.price.value);
+            switch(element.price.value){
+              
+              case "1" : {
+                this.price.value.push("$");
+                break;
+              }
+              case "2" : {
+                this.price.value.push("$$");
+                break;
+              }
+              case "3" : {
+                this.price.value.push("$$$");
+                break;
+              }
+              default: {
+                this.price.value.push("unkown");
+                break;
+              }
+            }
              break;
           }
          
@@ -64,19 +131,19 @@ export class SensorsDetailComponent implements OnInit {
           }
 
           case "phenomena" : {
-            this.phenomena.uri.push(element.phenomena.value);
-
+            
             if(element.unit != undefined){
-              this.phenomena.unit.push(element.unit.value);
+              this.phenomena.push({uri: element.phenomena.value, unit: element.unit.value, short: element.phenomena.value.slice(34)});
             }
             else{
-              this.phenomena.unit.push("unknown");
+              this.phenomena.push({uri: element.phenomena.value, unit: "unkown", short: element.phenomena.value.slice(34)});
             }
             break;
           }
 
           case "iri" : {
             this.uri.iri.push(element.iri.value);
+            this.uri.short.push(element.iri.value.slice(34))
             this.uri.labels.push(element.label.value);
             break;
           }
@@ -89,6 +156,7 @@ export class SensorsDetailComponent implements OnInit {
 
       });
       this.max = this.getMaxArrayLength();
+      console.log(this.uri);
       })
     });
 
@@ -98,11 +166,15 @@ export class SensorsDetailComponent implements OnInit {
   getMaxArrayLength(){
   return Array(Math.max(
     this.manufacturer.value.length, 
-    this.phenomena.uri.length,
-    this.phenomena.unit.length,
+    this.phenomena.length,
     this.uri.iri.length,
     this.uri.labels.length,
     this.uri.descriptions.length)).fill(0);
   }
+
+
+
+  
+
 
 }
--- src/app/sensors/sensors.component.ts
+++ src/app/sensors/sensors.component.ts
@@ -4,11 +4,13 @@ import { ApiService } from '../services/api.service';
 @Component({
   selector: 'senph-sensors',
   templateUrl: './sensors.component.html',
-  styleUrls: ['./sensors.component.scss']
+  styleUrls: ['./sensors.component.scss', '../app.component.scss']
 })
 export class SensorsComponent implements OnInit {
 
   sensorsArray;
+  selectedSensor;
+  sensorsArrayFiltered;
 
   constructor( private api:ApiService) { }
 
@@ -16,9 +18,30 @@ export class SensorsComponent implements OnInit {
     this.api.getSensors().subscribe(res => {
       this.sensorsArray=res;
       console.dir(this.sensorsArray);
+      this.assignCopy();
     });
   }
+
+  onSelect(sensor){
+    this.selectedSensor = sensor; 
+  }
+
   log(sensors) {
     console.dir(sensors);
+  }
+
+  assignCopy(){
+    this.sensorsArrayFiltered = Object.assign([], this.sensorsArray);
+ }
+  filterItem(value){
+      if(!value){
+          this.assignCopy();
+      } // when nothing has typed
+      this.sensorsArrayFiltered = Object.assign([], this.sensorsArray).filter(
+        sensors => JSON.stringify(sensors).toLowerCase().indexOf(value.toLowerCase()) > -1
+      )
+  }
+  createRoute(i){
+    return(['/sensor', this.sensorsArray[i].sensor.value.slice(34) ]);
   }
 }
