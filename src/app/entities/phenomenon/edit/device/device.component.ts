a0d1d361a55d1108344a0968d497f0b8aefdad7e
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-02 12:51:17 +0100

    create devices form component

---
+++ src/app/entities/phenomenon/edit/device/device.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../../../../services/api.service'
+
+@Component({
+  selector: 'senph-device',
+  templateUrl: './device.component.html',
+  styleUrls: ['./device.component.scss']
+})
+export class DeviceComponent implements OnInit {
+  @Input() parentForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private api: ApiService
+  ) { }
+  devicesArray;
+  validationMessages = {
+    'device': {
+      'required': 'Please select a device.'
+    } 
+  };
+
+  ngOnInit() {
+    this.retrieveDevices();
+  }
+
+  get device(): FormArray {
+    return this.parentForm.get('device') as FormArray;
+  } 
+
+  addDeviceButtonClick(): void {
+    (<FormArray>this.parentForm.get('device')).push(this.addDeviceFormGroup());
+  }
+
+  addDeviceFormGroup(): FormGroup {
+    return this.fb.group({
+      deviceUri: ['', [Validators.required]]
+    });
+  }
+
+  getSelectedDevice(id) {
+    return this.parentForm.value.device[id].deviceUri;
+  }
+
+  retrieveDevices() {
+    this.api.getDevices().subscribe(res => {
+      this.devicesArray = res;
+      this.devicesArray = this.devicesArray.filter(function (el) {
+        return el.device.type != 'bnode'
+      })
+      // console.log(this.devicesArray);
+      this.devicesArray.sort((a, b) => a.label[0].value.localeCompare(b.label[0].value));
+
+      // console.dir(this.devicesArray);
+    });
+  }
+
+  removeDeviceButtonClick(skillGroupIndex: number): void {
+    (<FormArray>this.parentForm.get('device')).removeAt(skillGroupIndex);
+  }
+}
+
