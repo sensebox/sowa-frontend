d21ec644c50c7c824020bd7bcebf8758f9597c2d
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-03 17:48:23 +0100

    create new detail page for phenomena using tiles

--- src/app/entities/phenomenon/phenomena-detail/phenomena-detail.component.ts
+++ src/app/entities/phenomenon/phenomena-detail/phenomena-detail.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from '../../../services/api.service';
 import { IPhenomenon } from '../../../interfaces/IPhenomenon';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { CustomValidators } from '../../../shared/custom.validators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../../services/api.service'
+import { IDomains } from '../../../interfaces/IDomains';
+import { IUnit } from '../../../interfaces/IUnit';
+import { ILabel } from 'src/app/interfaces/ILabel';
+import { LANGUAGES } from 'src/app/shared/mock-languages';
 
 @Component({
   selector: 'senph-phenomena-detail',
@@ -11,36 +17,110 @@ import { IPhenomenon } from '../../../interfaces/IPhenomenon';
 export class PhenomenaDetailComponent implements OnInit {
   phenomenon: IPhenomenon;
   uri;
+  languageArray = LANGUAGES;
 
 
   constructor(
-    private route:ActivatedRoute,
-    private api:ApiService,
-    private _routerService:Router
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private _routerService: Router
   ) { }
 
+  // ngOnInit() {
+  //   this.route.paramMap.subscribe(params => {
+  //     const shortUri = params.get('id');
+  //     if (shortUri) {
+  //       this.getPhenomenonDetails(shortUri);
+  //     }
+  //   });
+  // }
+
+
+
+
+  heroBannerString = "http://www.opensensemap.org/SENPH#";
+  phenomenonForm: FormGroup;
+  validationMessages = {
+    'uri': {
+      'required': 'URI is required.',
+      'uriSyntax': 'No white spaces allowed in URI.'
+    },
+    'label': {
+      'required': 'Label is required.'
+    },
+    'description': {
+      'required': 'Description is required.'
+    }
+  };
+
+  formErrors = {
+  };
+
   ngOnInit() {
+    // this.phenomenonForm = this.fb.group({
+    //   uri: ['', [Validators.required, CustomValidators.uriSyntax]],
+    //   label: this.fb.array([
+    //     this.addLabelFormGroup()
+    //   ]),
+    //   description: [''],
+    //   domain: this.fb.array([
+    //     this.addDomainFormGroup()
+    //   ]),
+    //   unit: this.fb.array([
+    //     this.addUnitFormGroup()
+    //   ])
+    // });
+
+    // this.phenomenonForm.valueChanges.subscribe(
+    //   (data) => {
+    //     this.logValidationErrors(this.phenomenonForm);
+    //   }
+    // );
+
+    // this.route.paramMap.subscribe(params => {
+    //   console.log(params)
+    //   const shortUri = params.get('iri');
+    //   console.log(shortUri)
+    //   if (shortUri) {
+    //     this.getPhenomenon(shortUri);
+    //   }
+    // });
+    // console.log(this.phenomenonForm.get('domain').controls);
+
     this.getPhenomenonDetails();
   }
 
 
   getPhenomenonDetails() {
-    return this.route.params.subscribe( res => {
+    return this.route.params.subscribe(res => {
       this.api.getPhenomenon(res.iri).subscribe((response: IPhenomenon) => {
+        console.log(response);
         this.phenomenon = response;
         this.uri = this.phenomenon.iri.value.slice(34);
         console.log(this.uri);
       });
     })
   }
-  
+
   redirectDomain(longURI) {
+    console.log(longURI);
     this._routerService.navigate(['/domain/', longURI.slice(34)]);
   }
 
-  editButtonClick(shortUri) {
-    this._routerService.navigate(['/phenomenon/edit', shortUri]);
+  editButtonClick(longUri) {
+    this._routerService.navigate(['/phenomenon/edit', longUri.slice(34)]);
   }
 
-
+  search(nameKey, myArray) {
+    console.log(nameKey)
+    for (var i = 0; i < myArray.length; i++) {
+      if (myArray[i].val === nameKey["xml:lang"]) {
+        console.log(myArray[i].show)
+        return myArray[i].show;
+      }
+    }
+  }
 }
+
+
