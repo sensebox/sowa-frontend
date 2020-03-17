5a80aa632bb56e113dd5f4328f70a1ddd2a5574b
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-17 04:01:07 +0100

    create new domains-details page and component

--- src/app/entities/domain/domains-detail/domains-detail.component.ts
+++ src/app/entities/domain/domains-detail/domains-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../../services/api.service';
+import { IDomain } from 'src/app/interfaces/IDomain';
+import { LANGUAGES } from 'src/app/shared/mock-languages';
+import { ILabel } from 'src/app/interfaces/ILabel';
+import { IPhenomena } from 'src/app/interfaces/IPhenomena';
 
 @Component({
   selector: 'senph-domains-detail',
@@ -9,53 +13,153 @@ import { ApiService } from '../../../services/api.service';
 })
 export class DomainsDetailComponent implements OnInit {
 
-  uri = [];
-  phenomena =[];
-  descriptions = [];
+  domain: IDomain;
+  uri;
+  languageArray = LANGUAGES;
+  domainHistory: Object;
+  historic = {
+    button1: undefined,
+    button2: undefined
+  };
+  prefLabel: ILabel;
+  phenomenaArray: any[];
 
 
 
   constructor(
-    private route:ActivatedRoute,
-    private api:ApiService
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private _routerService: Router
   ) { }
 
   ngOnInit() {
     this.getDomainDetails();
+    this.retrievePhenomena();
   }
 
   getDomainDetails() {
-    this.route.params.subscribe(res => {
-      this.api.getDomain(res.iri).subscribe((res:Array<any>) => {
-        res.forEach(element => {
-          switch (Object.getOwnPropertyNames(element)[0]) {
-  
-            case "description" : {
-                this.descriptions.push({value: element.description.value, lang: element.description["xml:lang"]});
-                break;
+    console.log(this._routerService.url)
+    if (this._routerService.url.search('/historic/') !== -1) {
+      this.historic.button1 = "Back to current version"
+    }
+    else {
+      this.historic.button1 = "Edit"
+      this.historic.button2 = "Log History"
+    };
+    console.log(this._routerService.url)
+    if (this.historic.button2) {
+      this.route.params.subscribe(res => {
+        console.log(res);
+        this.api.getDomain(res.iri).subscribe((response: IDomain) => {
+          console.log(response);
+          console.log("NEW");
+          this.domain = response;
+          this.domain.labels.forEach(element => {
+            console.log(element["xml:lang"])
+            if (element["xml:lang"] == "en") {
+              this.prefLabel = element
+              return
             }
-  
-            case "phenomena" : {
-              this.phenomena.push({iri: element.phenomena.value, label: element.phenomenaLabel.value, short: element.phenomena.value.slice(34)});
-              break;
-            }
-  
-            case "iri" : {
-              this.uri.push({ iri: element.iri.value, label: element.label.value, short: element.iri.value.slice(34)});
-              break;
-            }
-           
-            default: {
-               console.log("Invalid attribute");
-               break;
-            }
-         }
-  
+            this.prefLabel = element;
+          });
+          this.uri = this.domain.iri.value.slice(34);
+          // this.pushLabelNames(response);
+          console.log(this.uri);
         });
       })
-    })
+    }
+    else {
+      return this.route.params.subscribe(res => {
+        console.log(res);
+        this.api.getHistoricDomain(res.iri).subscribe((response: IDomain) => {
+          console.log(response);
+          console.log("historic");
+          this.domain = response;
+          this.domain.labels.forEach(element => {
+            console.log(element["xml:lang"])
+            if (element["xml:lang"] == "en") {
+              this.prefLabel = element
+              return
+            }
+            this.prefLabel = element;
+          });
+          this.uri = this.domain.iri.value.slice(34);
+          // this.pushLabelNames(response);
+          console.log(this.uri);
+        });
+      })
+    }
   }
-        // this.max = this.getMaxArrayLength();
-        // console.log(this.uri);
+
+  redirectDomain(longURI, link) {
+    this._routerService.navigate([link, longURI.slice(34)]);
+  }
+
+  button1(uri) {
+    console.log(this.historic)
+    if (this.historic.button2) {
+      this.editButtonClick(uri)
+    }
+    else {
+      this.route.params.subscribe(res => {
+        this._routerService.navigate(['/domain/detail/', res.uri]);
+      })
+    }
+  }
+  
+  editButtonClick(shortUri) {
+    this._routerService.navigate(['/domain/edit', shortUri]);
+  }
+
+  redirectHistoricDetails(uri, historicUri) {
+    this._routerService.navigate(['/domain/detail/' + uri + '/historic', historicUri.slice(34)]);
+  }
+
+  getHistory(shortUri) {
+    this.api.getDomainHistory(shortUri).subscribe(res => {
+      console.log(res);
+      this.domainHistory = res;
+    });
+  }
+
+  search(nameKey, val1, myArray, val2) {
+    // console.log(nameKey)
+    for (var i = 0; i < myArray.length; i++) {
+      // console.log(myArray[i][val1])
+      if (myArray[i][val1] === nameKey) {
+        console.log(myArray[i][val2]);
+        return myArray[i][val2];
+      }
+    }
+  }
+
+  searchPheno(nameKey, val1, myArray, val2) {
+    // console.log(nameKey)
+    for (var i = 0; i < myArray.length; i++) {
+      // console.log(myArray[i][val1])
+      if (myArray[i][val1].value === nameKey) {
+        console.log(myArray[i][val2].value);
+        return myArray[i][val2].value;
+      }
+    }
+  }
+
+
+  retrievePhenomena() {
+    this.api.getPhenomenaAllLabels().subscribe(res => {
+      console.log(res);
+      var tempArray: any = res;
+
+      tempArray = tempArray.filter(function (el) {
+        return el.phenomenon.type != 'bnode'
+      })
+      console.log(tempArray);
+      tempArray.sort((a, b) => a.phenomenonLabel.value.localeCompare(b.phenomenonLabel.value));
+      this.phenomenaArray = Array.from(tempArray, x => new IPhenomena(x));
+      console.log(this.phenomenaArray);
+    });
+  }
+  // this.max = this.getMaxArrayLength();
+  // console.log(this.uri);
 
 }
