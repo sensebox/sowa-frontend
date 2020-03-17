af2222d73c74682e4d79ab9fdf723a8dff7ec0a6
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-17 03:59:14 +0100

    create new list of domains page and component

--- src/app/entities/domain/domains/domains.component.ts
+++ src/app/entities/domain/domains/domains.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../services/api.service';
 import { Router } from '@angular/router';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { IDomain } from 'src/app/interfaces/IDomain';
+import { IDomains } from 'src/app/interfaces/IDomains';
 
 @Component({
   selector: 'senph-domains',
@@ -8,10 +11,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./domains.component.scss', '../../../app.component.scss']
 })
 export class DomainsComponent implements OnInit {
-  
+  faSearch = faSearch;
   domainsArray;
   domainsArrayFiltered;
   selectedDomain;
+  searchTerm;
+  pageLoadActive = "0";
 
 
   constructor( 
@@ -21,19 +26,19 @@ export class DomainsComponent implements OnInit {
 
   ngOnInit() {
     this.api.getDomains().subscribe(res => {
-      this.domainsArray=res;
-      this.domainsArray =  this.domainsArray.filter(function (el){
+      var tempArray: any = res;
+      tempArray =  tempArray.filter(function (el){
         return el.domain.type != 'bnode'
       })
-      // console.log(this.domainsArray);
-      this.domainsArray.sort((a,b) => a.label[0].value.localeCompare(b.label[0].value));
-
-      console.dir(res);
-      this.assignCopy();
+      // console.log(tempArray);
+      tempArray.sort((a,b) => a.label[0].value.localeCompare(b.label[0].value));
+      console.log(tempArray)
+      this.domainsArray = Array.from(tempArray, x => new IDomains(x));
     });
   }
 
   onSelect(domain){
+    this.acitivatePageLoad();
     this.selectedDomain = domain; 
     this._routerService.navigate(['/domain/detail/', domain.domain.value.slice(34)]);
 
@@ -50,7 +55,13 @@ export class DomainsComponent implements OnInit {
         sensors => JSON.stringify(sensors).toLowerCase().indexOf(value.toLowerCase()) > -1
       )
   }
+
+
   createRoute(i){
     return(['/domain', this.domainsArray[i].domain.value.slice(34) ]);
+  }
+
+  acitivatePageLoad() {
+    this.pageLoadActive = "1";
   }
 }
