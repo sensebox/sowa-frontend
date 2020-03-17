43173407bb71c66bdd38bc2c27cb8a056e1e5519
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-17 04:04:59 +0100

    implement create devices page and component (devices-new)

--- src/app/entities/device/device-new/device-new.component.ts
+++ src/app/entities/device/device-new/device-new.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
 import { CustomValidators } from '../../../shared/custom.validators';
-import { ActivatedRoute } from '@angular/router';
-import { Device } from '../../../phenomenon';
-import { ApiService } from '../../../services/api.service'
-import { ISensors } from '../../../interfaces/ISensors';
-
+import { ILabel } from 'src/app/interfaces/ILabel';
+import { ISensors } from 'src/app/interfaces/ISensors';
 
 @Component({
   selector: 'senph-device-new',
@@ -15,58 +14,57 @@ import { ISensors } from '../../../interfaces/ISensors';
 export class DeviceNewComponent implements OnInit {
   heroBannerString = "http://www.opensensemap.org/SENPH#";
   deviceForm: FormGroup;
+
   validationMessages = {
     'uri': {
       'required': 'URI is required.',
       'uriSyntax': 'No white spaces allowed in URI.'
     },
-    'name': {
-      'required': 'URI is required.'
+    'label': {
+      'required': 'Label is required.'
     },
     'description': {
       'required': 'Description is required.'
     },
     'website': {
-      'required': 'website is required.'
-    },
-    'image': {
-      'required': 'image is required.'
+      'required': 'Provide a datasheet link or use the checkbox to set its value to undefined.',
+      'uriSyntax': 'No white spaces allowed in Datasheet-URL.'
     },
     'contact': {
-      'required': 'contact is required.'
+      'required': 'Provide a lifeperiod or use the checkbox to set its value to undefined.'
+    },
+    'image': {
+      'required': 'Provide an image link or use the checkbox to set its value to undefined.',
+      'uriSyntax': 'No white spaces allowed in Image-URL.'
     }
   };
 
   formErrors = {
-    // 'uri': '',
-    // 'name': '',
-    // 'description': '',
-    // 'website': '',
-    // 'image': '',
-    // 'contact': '',
-    // 'sensorUri': '',
-    // 'phenomenon': ''
   };
+  shortUri: string;
+  submitted = false;
 
-  constructor(private fb: FormBuilder,
+
+  constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
-    private api: ApiService) { }
+    private api: ApiService,
+    private _routerService: Router
+  ) { }
 
   ngOnInit() {
     this.deviceForm = this.fb.group({
       uri: ['', [Validators.required, CustomValidators.uriSyntax]],
-      name: ['', [Validators.required]],
+      label: this.fb.array([
+        this.addLabelFormGroup()
+      ]),
       description: ['', [Validators.required]],
-      website: ['', [Validators.required]],
-      image: ['', [Validators.required]],
-      contact: ['', [Validators.required]],
+      website: [{ value: '', disabled: false }, [Validators.required, CustomValidators.uriSyntax]],
+      image: [{ value: '', disabled: false }, [Validators.required, CustomValidators.uriSyntax]],
+      contact: [{ value: '', disabled: false }, [Validators.required]],
       sensor: this.fb.array([
         this.addSensorFormGroup()
       ])
-      // sensor: this.fb.group({
-      //   sensorUri: ['', [Validators.required]],
-      //   phenomenon: ['', [Validators.required]]
-      // })
     })
 
     this.deviceForm.valueChanges.subscribe(
@@ -74,31 +72,18 @@ export class DeviceNewComponent implements OnInit {
         this.logValidationErrors(this.deviceForm);
       }
     );
-
   }
 
-  addSensorButtonClick(): void {
-    (<FormArray>this.deviceForm.get('sensor')).push(this.addSensorFormGroup());
-  }
 
   logValidationErrors(group: FormGroup = this.deviceForm): void {
-    // console.log(Object.keys(group.controls));
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
       if (abstractControl instanceof FormGroup) {
         this.logValidationErrors(abstractControl);
       }
-      // else if (abstractControl instanceof FormArray) {
-      //   for (const control of abstractControl.controls) {
-      //     if (control instanceof FormGroup) {
-      //       this.logValidationErrors(control)
-      //     }
-      //   }
-      // }
-
       else {
         this.formErrors[key] = '';
-        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty || abstractControl.value !== '')) {
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty || abstractControl.value !== '' || this.submitted)) {
           const messages = this.validationMessages[key];
           for (const errorKey in abstractControl.errors) {
             if (errorKey) {
@@ -110,66 +95,43 @@ export class DeviceNewComponent implements OnInit {
     });
   }
 
-  onLoadButtonClick() {
-    // this.deviceForm.patchValue({
-    //   uri: "senseBox",
-    //   name: "senseBox",
-    //   description: "A DIY kit to build an environmental monitoring station.",
-    //   website: "www.sensebox.de",
-    //   image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjCosjcu-biAhXLJFAKHWe1CEIQjRx6BAgBEAU&url=https%3A%2F%2Fsensebox.de%2F&psig=AOvVaw1FMe0n1pkfzfow3lo3uqOP&ust=1560515579903367",
-    //   contact: "Reedu GmbH",
-    //   sensor: {
-    //     sensorUri: "HDC1080",
-    //     phenomenon: "temperature"
-    //   }
-    // });
-    // this.logValidationErrors(this.deviceForm);
-    // console.log(this.formErrors);
-    const formGroup = this.fb.group([
-      new FormControl('sensorUri', Validators.required),
-      new FormControl('sensorLabel', Validators.required),
-    ]);
 
-    const formArray = this.fb.array([
-      new FormControl('sensorUri', Validators.required),
-      new FormControl('sensorLabel', Validators.required),
-    ]);
-
-    console.log(formArray);
-    console.log(formGroup);
-  }
-
-  onSubmit() {
-    console.log(this.deviceForm.value);
-    this.api.addDevice(this.deviceForm.value).subscribe(res => { console.log(res) });
-    // this.diagnostic(this.deviceForm);
+  addLabelFormGroup(): FormGroup {
+    return this.fb.group({
+      type: "literal",
+      value: ['', [Validators.required]],
+      lang: ['', [Validators.required]]
+    });
   }
 
   addSensorFormGroup(): FormGroup {
     return this.fb.group({
-      sensorUri: ['', [Validators.required]],
-      sensorLabel: ['', [Validators.required]]
+      sensorUri: ['', [Validators.required]]
     });
   }
 
-  removeSensorButtonClick(skillGroupIndex: number): void {
-    (<FormArray>this.deviceForm.get('sensor')).removeAt(skillGroupIndex);
+
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.deviceForm.getRawValue());
+
+    if (this.deviceForm.invalid) {
+      console.log("invalid");
+    }
+    else {
+      console.log("valid");
+      this.api.createDevice(this.deviceForm.getRawValue()).subscribe(
+        res => {
+          console.log(res)
+        },
+        (error: any) => console.log(error)
+      );
+      // this.diagnostic(this.deviceForm);
+    }
   }
-
-
-
-  setExistingSensors(sensorSet: ISensors[]): FormArray {
-    const formArray = new FormArray([]);
-    sensorSet.forEach(s => {
-      formArray.push(this.fb.group({
-        sensorUri: s.sensor.value,
-        sensorLabel: s.sensorLabel.value
-      }));
-    });
-
-    return formArray;
-  }
-
 }
+
+
 
 
