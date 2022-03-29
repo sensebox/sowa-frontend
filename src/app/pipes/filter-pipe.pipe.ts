0a539f507e45934ad82319156e53c154fe31b9d7
fab-scm <fabian.schumacher@uni-muenster.de>
2022-03-29 21:52:56 +0200

    fixed filter pipe again

--- src/app/pipes/filter-pipe.pipe.ts
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -8,19 +8,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipePipe implements PipeTransform {
 
   transform(filterArray: Object[], searchTerm: string): Object[] {
-    console.log(filterArray)
+    // console.log(filterArray)
     if (!filterArray || !searchTerm) {
       return filterArray;
     }
-    console.log(filterArray)
+    // console.log(filterArray)
     return filterArray.filter(element => {
-      // console.log(element)
-      let translationArray = Object.values(element)[1];
-      console.log(translationArray)
+      // console.log(element["label"].item)
+      let translationArray = element["label"].item;
+      // console.log(translationArray)
       if (translationArray.length > 1) {
         for (let i = 0; i < translationArray.length; i++) {
           if(translationArray[i].languageCode === "en") {
-            console.log(translationArray[i].text);
+            // console.log(translationArray[i].text);
             return (translationArray[i].text.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
           }
         }
